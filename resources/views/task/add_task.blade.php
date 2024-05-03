182cab96c300d78aa587d2b805ad92d8e59f5717
siamshaeed <siamshaeed@gmail.com>
2024-05-03 22:43:55 +0600

    frontend complete

--- resources/views/task/add_task.blade.php
+++ resources/views/task/add_task.blade.php
@@ -13,18 +13,28 @@
         <div class="col-md-8">
             <div class="card mt-3">
                 <div class="card-header">
-                  <h2>Add Task</h2>
+                    <div class="row">
+                        <div class="col-md-10"><h2>Add Task</h2></div>
+                        <div class="col-md-2"><a href="{{ route('taskList')}}" type="button" class="btn btn-sm btn-secondary">Task List</a></div>
+                    </div>
                 </div>
                 <div class="card-body">
                     <form>
                         <div class="form-group">
-                          <label for="exampleInputEmail1">Email address</label>
-                          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                          <label for="title">Title</label>
+                          <input type="text" class="form-control" id="title" placeholder="Enter Task Title">
                         </div>
+
                         <div class="form-group">
-                          <label for="exampleInputPassword1">Password</label>
-                          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                            <label for="title">Description</label>
+                            <input type="text" class="form-control" id="title" placeholder="Enter Task Description">
                         </div>
+
+                        <div class="form-group">
+                          <label for="title">Long Description</label>
+                          <textarea name="long_description" class="form-control" cols="30" rows="4" placeholder="Enter Task Long Description"></textarea>
+                        </div>
+
                         <div class="col-md-2"><a href="#" type="button" class="btn btn-sm btn-secondary mt-3">Save</a></div>
                       </form>
                 </div>
